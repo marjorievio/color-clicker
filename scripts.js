787e59a1d8d162a849bba56929304d85c113e24d
Marjorie Vio <marjorievio1@gmail.com>
2021-07-19 19:21:28 -0400

    actividad color-clicker

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function random_color()
+          {
+             var rgb = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+             color = '#'  //this is what we'll return!
+             for(var i = 0; i < 6; i++) 
+             {
+                x = Math.floor((Math.random()*16))
+                color += rgb[x]; 
+             }
+             return color;
+          }
+          $(document).ready(function(){
+             $('#large_box').click(function(){
+                alert('you clicked the big box!');  
+         //comenta esto cuando hayas averiguado para qué se utiliza event.stopPropagation 
+             })
+             $('.side_box').click(function(event){
+                event.stopPropagation();
+             })
+             $('.middle_box').click(function(event){
+                event.stopPropagation();
+             })
+          });
+      
